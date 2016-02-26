a188b344946d4d5f99be7f453eddb119ee5cbe82
Jim McAndrew <jim@loc8.us>
2016-02-26 08:56:42 -0700

    Fix tests: Update examples/kml_to_json.js

--- examples/kml_to_json.js
+++ examples/kml_to_json.js
@@ -1,29 +1,27 @@
-var libxmljs = require('libxmljs'),
-  xmljs_translator = require('../'),
-  http = require('http'),
+var xmljs_translator = require('../'),
+  http = require('https'),
   options = {
     'host': 'www.cityofmesquite.com',
-    'port': '80',
-    'path': '/gis/KML/Fire_Stations.kml'
+    'path': '/DocumentCenter/Home/View/694'
   };
 
 // Utility function that downloads a URL and invokes
 // callback with the data.
-function download(options, callback) {
-  http.get(options, function(res) {
-    var data = "";
-    res.on('data', function(chunk) {
+function download (options, callback) {
+  http.get(options, function (res) {
+    var data = '';
+    res.on('data', function (chunk) {
       data += chunk;
     });
-    res.on("end", function() {
+    res.on('end', function () {
       callback(data);
     });
-  }).on("error", function() {
+  }).on('error', function () {
     callback(null);
   });
 }
 
-function convertData(data) {
+function convertData (data) {
   console.log(JSON.stringify(xmljs_translator.jsonify(data), null, 2));
 }
 
