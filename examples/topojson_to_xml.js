65b68bdedb26562b104f95b79fdac90d8febab2f
Jim McAndrew <jim@loc8.us>
2016-02-26 08:56:42 -0700

    Fix tests: Update examples/topojson_to_xml.js

--- examples/topojson_to_xml.js
+++ examples/topojson_to_xml.js
@@ -1,30 +1,33 @@
-var libxmljs = require('libxmljs'),
-  xmljs_translator = require('../'),
+var xmljs_translator = require('../'),
   http = require('https'),
   options = {
-    'host': 'raw.github.com',
+    'host': 'raw.githubusercontent.com',
     'port': '443',
     'path': '/mbostock/topojson/master/examples/us-10m.json'
   };
 
 // Utility function that downloads a URL and invokes
 // callback with the data.
-function download(options, callback) {
-  http.get(options, function(res) {
-    var data = "";
-    res.on('data', function(chunk) {
+function download (options, callback) {
+  http.get(options, function (res) {
+    var data = '';
+    res.on('data', function (chunk) {
       data += chunk;
     });
-    res.on("end", function() {
-      callback(data);
+    res.on('end', function () {
+      callback(null, data);
     });
-  }).on("error", function() {
-    callback(null);
+  }).on('error', function (e) {
+    callback(e);
   });
 }
 
-function convertData(data) {
-  console.log(xmljs_translator.xmlify(JSON.parse(data)));
+function convertData (err, data) {
+  if (!err) {
+    console.log(xmljs_translator.xmlify(JSON.parse(data)));
+  } else {
+    throw err;
+  }
 }
 
 download(options, convertData);
